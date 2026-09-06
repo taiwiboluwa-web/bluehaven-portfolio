0782474df8312304a13e7531fdff058289d2f5db
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-09-06 09:32:56 +0100

    feat: route admin dashboard at /admin

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '../app/App'
+import { AdminDashboard } from '../app/components/AdminDashboard'
 import '../index.css'
 import './cms-live-media.css'
 import './cms-live-media'
 import './bluehaven-ai'
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!)
+const isAdmin = window.location.pathname.replace(/\/+$/, '') === '/admin'
+
+root.render(
   <StrictMode>
-    <App />
+    {isAdmin ? <AdminDashboard /> : <App />}
   </StrictMode>,
 )
