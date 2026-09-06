d2d51e9346168a6533b2c1b99284b34c3a052b21
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-09-06 16:33:11 +0100

    remove Buddy from public app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import Admin from './app/Admin.tsx';
 import SiteEnhancements from './app/SiteEnhancements.tsx';
-import ActiveBuddy from './app/components/ActiveBuddy.tsx';
 import './styles/index.css';
 
 const root = createRoot(document.getElementById('root')!);
 const isAdmin = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/');
-root.render(isAdmin ? <Admin /> : <><App /><SiteEnhancements /><ActiveBuddy /></>);
+root.render(isAdmin ? <Admin /> : <><App /><SiteEnhancements /></>);
