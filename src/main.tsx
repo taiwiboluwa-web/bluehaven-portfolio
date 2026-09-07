a75a1e852e7092b93e3b73fd54770691a5c750f5
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-09-07 19:49:00 +0100

    perf: remove unused eager application imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import App from './app/App.tsx';
 import Admin from './app/Admin.tsx';
 import Stories from './app/Stories.tsx';
 import RouteView from './app/RouteView.tsx';
 import NavigationEnhancement from './app/NavigationEnhancement.tsx';
-import SiteEnhancements from './app/SiteEnhancements.tsx';
 import './styles/index.css';
 
 const root = createRoot(document.getElementById('root')!);
